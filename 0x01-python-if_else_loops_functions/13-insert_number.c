af0e08b80f26b56f18857103e4a6e21988c21f4d
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-20 20:57:06 -0700

    feat: linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,1 +1,12 @@
 #include "lists.h"
+
+/**
+ * insert_node - insert a node into a linked list
+ * @head: head node
+ * @number: value of node
+ * Return: address of node of success, NULL if fail
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	return (NULL)
+}
