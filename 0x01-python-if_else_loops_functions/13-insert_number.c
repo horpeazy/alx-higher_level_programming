de666ccade17ae7372e425d7ba78c1b9e35998e4
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-20 21:39:01 -0700

    fix: hot fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,27 +15,19 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	prev_node = NULL;
 	temp_node = *head;
-	if (!temp_node)
+	if (temp_node == NULL)
 	{
 		new_node->n = number;
 		new_node->next = NULL;
 		return (new_node);
 	}
-	while (temp_node)
+	while (temp_node->n < number)
 	{
-		if (temp_node->n >= number)
-		{
-			if (prev_node != NULL)
-				prev_node->next = new_node;
-			new_node->n = number;
-			new_node->next = temp_node;
-			return (new_node);
-		}
 		prev_node = temp_node;
 		temp_node = temp_node->next;
 	}
-	prev_node->next = new_node;
 	new_node->n = number;
-	new_node->next = NULL;
+	new_node->next = temp_node;
+	prev_node->next = new_node;
 	return (new_node);
 }
