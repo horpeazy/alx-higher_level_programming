73f7af185d54fd91db2cb996a6b5e3ebaebd44df
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-20 21:55:46 -0700

    feat: 6-pritn com

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
+        if(i == j):
+            continue
+        elif( i > j):
+            continue
+        elif(i == 8 and j == 9):
+            print(89)
+        else:
+            print("{}{}".format(i, j), end=", ")
