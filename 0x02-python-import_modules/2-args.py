fab16a2d811b9c18e947321d69af916a048f88da
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-22 18:18:26 -0700

    feat: 2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+args = len(sys.argv)
+
+if(args == 1):
+    print("0 argumets.")
+elif(args == 2):
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments".format(args - 1))
+    for i in range(1, args):
+        print("{}: {}".format(i, sys.argv[i]))
