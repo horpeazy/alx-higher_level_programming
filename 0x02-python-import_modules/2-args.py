fe2619f2340a0c01f0809c6ac5a037198bb51fe9
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-22 18:27:13 -0700

    fix: column

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments".format(args - 1))
+        print("{} arguments:".format(args - 1))
         for i in range(1, args):
             print("{}: {}".format(i, sys.argv[i]))
