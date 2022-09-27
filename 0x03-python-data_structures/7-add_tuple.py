3a88c133ffcc66210ab17c1bc8dce972cecf5b37
horpeazy <iyamuhope.nosa647@gmail.com>
2022-09-26 18:18:12 -0700

    style: over indentation

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-     a_1 = 0
-     a_2 = 0
-     b_1 = 0
-     b_2 = 0
-     len_a = len(tuple_a)
-     len_b = len(tuple_b)
+    a_1 = 0
+    a_2 = 0
+    b_1 = 0
+    b_2 = 0
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
 
-     if(len_a >= 2):
-         a_1 = tuple_a[0]
-         a_2 = tuple_a[1]
-     if(len_a == 1):
-         a_1 = tuple_a[0]
-     if(len_b == 2):
-         b_1 = tuple_b[0]
-         b_2 = tuple_b[1]
-     if(len_b == 1):
-         b_1 = tuple_b[0]
+    if(len_a >= 2):
+        a_1 = tuple_a[0]
+        a_2 = tuple_a[1]
+    if(len_a == 1):
+        a_1 = tuple_a[0]
+    if(len_b >= 2):
+        b_1 = tuple_b[0]
+        b_2 = tuple_b[1]
+    if(len_b == 1):
+        b_1 = tuple_b[0]
 
-     my_tuple = (a_1 + b_1, a_2 + b_2)
-     return my_tuple
-
+    my_tuple = (a_1 + b_1, a_2 + b_2)
+    return my_tuple
