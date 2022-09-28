b1b5ea491c2c622cd5c410cb63350f8bfb01cc1f
horpeazy <iyamuhope.nosa@gmail.com>
2022-09-28 16:22:38 -0700

    fix: zero division

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,10 +4,11 @@ def weight_average(my_list=[]):
     total = 0
     avg = 0
 
+    if not my_list:
+        return 0
+
     for tul in my_list:
         total += tul[0] * tul[1]
         avg += tul[1]
 
     return total/avg
-
-
