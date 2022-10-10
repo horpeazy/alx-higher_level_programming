dfcdc82a486106d5859dd3261a3a600b115a854c
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-11 00:51:25 +0200

    feat: format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while i < x:
-            print(my_list[i], end="" if i != x -1 else '\n')
+            print("{:d}".format(my_list[i]), end="" if i != x -1 else '\n')
             i += 1
     except Exception as e:
         print("")
