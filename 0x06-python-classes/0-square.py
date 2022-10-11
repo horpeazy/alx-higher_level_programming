19ef4e05dd18d2f2580261e675d8dec6353bdf10
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-11 15:13:58 +0200

    feat: 0-square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+"""
+    This modules defines a python class Square
+    that contains no attribute
+"""
+
+
+class Square:
+    """
+        This a class that represents a square
+
+        Arguments:
+            It takes no arguments
+    """
+    pass
