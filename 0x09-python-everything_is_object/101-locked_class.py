de8bd0a558183defd9d72a23ed99d1d51cf5cdee
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-18 18:25:41 +0200

    feat: slot object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-class LockedClass(object):
-    def __setattr__(self, __name: str, __value) -> None:
-        if __name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
-        object.__setattr__(self, __name, __value)
+class LockedClass:
+    
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name="") -> None:
+        self.first_name = first_name
