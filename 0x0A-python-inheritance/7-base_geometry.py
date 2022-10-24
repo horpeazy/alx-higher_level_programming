5ece46f7b9431d9898bcb1be278dc56454716758
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-24 22:13:09 +0200

    feat: rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,42 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 
-"""
-    This module supplies a class BaseGeometry
+# """
+#     This module supplies a class BaseGeometry
+# """
+
+
+# class BaseGeometry:
+#     """An bare bones base geometry class"""
+
+#     def area(self):
+#         """raise an Exception"""
+#         raise Exception("area() is not implemented")
+
+#     def integer_validator(self, name, value):
+#         """validates value"""
+#         if type(value) is not int:
+#             raise TypeError("{} must be an integer".format(name))
+#         if value <= 0:
+#             raise ValueError("{} must be greater than 0".format(name))
+#!/usr/bin/python3
+"""Module 7-base_geometry.
+Creates a BaseGeometry class.
 """
 
 
 class BaseGeometry:
-    """An bare bones base geometry class"""
+    """Class with public instance methods."""
 
     def area(self):
-        """raise an Exception"""
-        raise Exception("area() is not implemented")
+        """Raises an Exception with the message
+        'area() is not implemented'.
+        """
+
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """validates value"""
+        """Validates value."""
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
