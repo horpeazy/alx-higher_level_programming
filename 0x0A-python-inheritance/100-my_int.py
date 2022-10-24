0f1155d94ed8c45eb62070f62a40352fccea566f
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-24 22:48:20 +0200

    feat: rectangle

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,12 @@
 """
 
 class MyInt(int):
+    """Custome int class"""
 
     def __eq__(self, __x: object) -> bool:
+        """rebel version of __eq__"""
         return not super().__eq__(__x)
 
     def __ne__(self, __x: object) -> bool:
+        """rebel version of __ne__"""
         return not super().__ne__(__x)
