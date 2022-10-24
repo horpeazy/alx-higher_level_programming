2e3ec8b91d0f63061cb2aef6350cafd01caf1874
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-24 22:25:34 +0200

    feat: rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,9 +11,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """A Square class"""
     def __init__(self, size) -> None:
+        self.integer_validator("size", size)
         super.__init__(size, size)
         self.__size = size
-        self.integer_validator("size", size)
 
     def area(self):
         return self.__size ** 2
