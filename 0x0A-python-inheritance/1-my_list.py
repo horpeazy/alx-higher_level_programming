c50ea55e1f31488590d0429f502492ae6f0bff1a
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-24 13:01:42 +0200

    feat: my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    This module supplies a class MyList that inherits for the
+    list class
+"""
+
+
+class MyList(list):
+    """
+        A custom class that inherits from the python class
+    """
+    def __init_subclass__(cls) -> None:
+        """Initialize the class with parent attr and methods"""
+        return super().__init_subclass__()
+
+    def print_sorted(self):
+        """prints the elements in reversed sorted order"""
+        print(sorted(self))
