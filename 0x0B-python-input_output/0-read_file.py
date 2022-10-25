91f31aa2ddcc8374bcdffa93ea11cf610f6fb753
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 12:52:27 +0200

    fix: encode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,6 @@
 
 def read_file(filename=""):
     """Reads a file and prints it to stdout"""
-    if not filename:
-        return
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f.readlines():
             print(line)
