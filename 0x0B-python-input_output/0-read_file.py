1b84d961dfb1c0f324f04114f6e56eff619b58a4
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 12:20:53 +0200

    fix: encode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
     This module supplies a function that performs I/O
 """
 
+
 def read_file(filename="", encode="utf8"):
     """Reads a file and prints it to stdout"""
     with open(filename) as f:
