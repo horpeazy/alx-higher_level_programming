69159ae486ac0bc5e4f5df7b2023fd4659956493
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 23:03:43 +0200

    feat: pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This module supplies a function pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    if n == 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+
+    prev_res = pascal_triangle(n-1)
+    prev_arr = prev_res[-1]
+    new_arr = [1]
+
+    for i in range(1, len(prev_arr)):
+        new_arr.append(prev_arr[i] + prev_arr[i - 1])
+    new_arr.append(1)
+    prev_res.append(new_arr)
+
+    return prev_res
