2c67cb4720bd1359010b67d2881643da94ec65c2
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 15:09:30 +0200

    feat: add item script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+# save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+# load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 new_list = []
 for i in range(1, len(sys.argv)):
     new_list.append(sys.argv[i])
 
-# try:
-#     old_list = load_from_json_file("add_item.json")
-#     old_list.extend(new_list)
-#     save_to_json_file(old_list, "add_item.json")
-# except Exception:
-#     save_to_json_file(new_list, "add_item.json")
+try:
+    old_list = load_from_json_file("add_item.json")
+    old_list.extend(new_list)
+    save_to_json_file(old_list, "add_item.json")
+except Exception:
+    save_to_json_file(new_list, "add_item.json")
