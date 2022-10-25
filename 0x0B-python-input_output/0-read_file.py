dd197f7c0ebe9174073e1dc5ce838bfe03881e51
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 12:17:10 +0200

    feat: read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module supplies a function that performs I/O
+"""
+
+def read_file(filename=""):
+    """Reads a file and prints it to stdout"""
+    with open(filename) as f:
+        file_content = f.read()
+        print(file_content)
