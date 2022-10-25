454ba9be74b6c673dcc5431c23ecfb3b408c4fae
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 20:32:40 +0200

    feat: add item script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""
+    Add all arguments to a python list, and
+    then save them to a file
+"""
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 new_list = []
-if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv)):
-        new_list.append(sys.argv[i])
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
 
 try:
     old_list = load_from_json_file("add_item.json")
