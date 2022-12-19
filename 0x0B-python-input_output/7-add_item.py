f30dcd75c4942df5cef9348c7b08224fa2918062
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-19 15:24:45 -0800

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+from "5-save_to_json_file" import save_to_json_file
+from "6-load_from_json_file" import load_from_json_file
 
 new_list = []
 for i in range(1, len(sys.argv)):
