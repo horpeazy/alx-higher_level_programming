e21f4f23ec90219043a7fe5a411156cf134c907f
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-19 17:05:06 -0800

    feat; Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let myArray = [];
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+    for (let i = 2; i < process.argv.length; i++) {
+      myArray.push(parseInt(process.argv[i]));
+    }
+  myArray.sort();
+  console.log(myArray[myArray.length - 2]);
+}
