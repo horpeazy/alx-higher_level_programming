c4dd13ff49636ee0ebcef81354f361eb7bcc14d3
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-19 16:55:24 -0800

    feat: Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (!number || number <= 1) {
+    return (1);
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
