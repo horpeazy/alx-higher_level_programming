f7b0c123f057a102b501c1c9d7b4e59c781bb381
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-23 05:05:09 -0800

    feat: update field

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This modules uses sqlachemy to query the database """
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    # create an engine
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.add(state)
+    session.commit()
+    session.close()
