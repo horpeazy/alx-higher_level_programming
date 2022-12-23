4fd03938fcc27444f37264d582479d6857539688
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-22 20:09:50 -0800

    feat: sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     import sys
 
     # create an engine
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Base.metadata.create_all(engine)
 
@@ -21,5 +22,3 @@ if __name__ == "__main__":
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
-
-
