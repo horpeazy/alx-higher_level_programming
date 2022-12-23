1f5020ee3902cb2ed9533d115f0984f84d1883ff
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-22 17:52:56 -0800

    feat: Cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,12 +10,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name\
+    cur.execute("SELECT cities.name, states.name\
     FROM cities JOIN states ON states.id = cities.state_id \
     WHERE states.name = (%s);", (sys.argv[4],))
     cities = cur.fetchall()
     for i in range(len(cities)):
-        if i == len(cities) - 1:
-            print(cities[i][0])
-        else:
-            print(cities[i][0], end=", ")
+        if cities[i][1] == sys.argv[4]:
+            if i == len(cities) - 1:
+                print(cities[i][0])
+            else:
+                print(cities[i][0], end=", ")
