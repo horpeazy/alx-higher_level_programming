0893f3a00cf3b9f486cae301eef540670959b852
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-22 17:49:03 -0800

    feat: Cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name\
+    FROM cities JOIN states ON states.id = cities.state_id \
+    WHERE states.name = (%s);", (sys.argv[4],))
+    cities = cur.fetchall()
+    for i in range(len(cities)):
+        if i == len(cities) - 1:
+            print(cities[i][0])
+        else:
+            print(cities[i][0], end=", ")
