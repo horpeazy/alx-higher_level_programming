adbbcf9a4f304605489f70ae1e5ec32aae1c6880
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-22 20:59:11 -0800

    feat: etst

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This modules uses sqlachemy to query the database """
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    # create an engine
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+
+    state = session.query(State).first()
+    if state:
+        print("1: {}".format(state.name))
+    else:
+        print()
