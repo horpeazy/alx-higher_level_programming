72de003c0b1139ef4003050c992f524689eb86a5
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-23 07:11:11 -0800

    feat: join

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     # create a Session
     session = Session()
 
-    states = session.query(State).outerjoin(City).all()
+    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
