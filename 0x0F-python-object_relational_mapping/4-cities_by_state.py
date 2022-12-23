36e38858445ed3cd9c2e60b9e6a02dbb27641e71
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-22 17:41:46 -0800

    feat: SQL injection safe

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * from cities;")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON states.id = cities.state_id;")
     for row in cur.fetchall():
         print(row)
